6d0d37a9206579204a8944d7c13b499642661283
Hamza Bouyahya <hamzabouyahya198@gmail.com>
2022-01-04 15:15:33 +0100

    fix the dark mode : remove the profile item instead of clearing the localestorage

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,21 +1,21 @@
-import { AUTH, AUTHERROR, LOGOUT } from '../constants/actionTypes'
+import { AUTH, AUTHERROR, LOGOUT } from "../constants/actionTypes";
 
 const authReducuer = (state = { authData: null }, action) => {
-    switch (action.type) {
-        case AUTH:
-            localStorage.setItem('profile', JSON.stringify({ ...action?.data }))
-            return { ...state, authData: action?.data }
+  switch (action.type) {
+    case AUTH:
+      localStorage.setItem("profile", JSON.stringify({ ...action?.data }));
+      return { ...state, authData: action?.data };
 
-        case AUTHERROR:
-            return { ...state, error: action.payload };
+    case AUTHERROR:
+      return { ...state, error: action.payload };
 
-        case LOGOUT:
-            localStorage.clear()
-            return { ...state, authData: null }
+    case LOGOUT:
+      window.localStorage.removeItem("profile");
+      return { ...state, authData: null };
 
-        default:
-            return state;
-    }
-}
+    default:
+      return state;
+  }
+};
 
-export default authReducuer
+export default authReducuer;
