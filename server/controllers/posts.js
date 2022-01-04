59fd033ce6d8e02a0219440e1c4fde0c8e84ffb0
Hamza Bouyahya <hamzabouyahya198@gmail.com>
2022-01-04 17:42:29 +0100

    make the controller of commenting post better

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -88,12 +88,16 @@ export const likePost = async (req, res) => {
 };
 
 export const commentPost = async (req, res) => {
-  const { id } = req.params;
-  const { value } = req.body;
-  const post = await PostMessage.findById(id);
-  post.comments.push(value);
-  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
-    new: true,
-  });
-  res.json(updatedPost);
+  try {
+    const { id } = req.params;
+    const { value } = req.body;
+    const post = await PostMessage.findById(id);
+    post.comments.push(value);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+      new: true,
+    });
+    res.json(updatedPost);
+  } catch (error) {
+    console.log(error);
+  }
 };
